516097549c9b4d06e06186471ee4d3895e1b0717
LeeSmet <lee.smet@hotmail.com>
2020-06-24 17:23:49 +0200

    Extend workload types

--- models/generated/workloads/gateway.go
+++ models/generated/workloads/gateway.go
@@ -11,6 +11,17 @@ type GatewayProxy struct {
 	Port       uint32    `bson:"port" json:"port"`
 	PortTLS    uint32    `bson:"port_tls" json:"port_tls"`
 	PoolId     int64     `bson:"pool_id" json:"pool_id"`
+
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (g GatewayProxy) WorkloadID() int64 {
@@ -24,6 +35,17 @@ type GatewayReverseProxy struct {
 	Domain     string    `bson:"domain" json:"domain"`
 	Secret     string    `bson:"secret" json:"secret"`
 	PoolId     int64     `bson:"pool_id" json:"pool_id"`
+
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (g GatewayReverseProxy) WorkloadID() int64 {
@@ -37,6 +59,17 @@ type GatewaySubdomain struct {
 	Domain     string    `bson:"domain" json:"domain"`
 	IPs        []string  `bson:"ips" json:"ips"`
 	PoolId     int64     `bson:"pool_id" json:"pool_id"`
+
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (g GatewaySubdomain) WorkloadID() int64 {
@@ -49,6 +82,17 @@ type GatewayDelegate struct {
 	NodeId     string    `bson:"node_id" json:"node_id"`
 	Domain     string    `bson:"domain" json:"domain"`
 	PoolId     int64     `bson:"pool_id" json:"pool_id"`
+
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (g GatewayDelegate) WorkloadID() int64 {
@@ -61,6 +105,17 @@ type Gateway4To6 struct {
 	NodeId     string    `bson:"node_id" json:"node_id"`
 	PublicKey  string    `bson:"public_key" json:"public_key"`
 	PoolId     int64     `bson:"pool_id" json:"pool_id"`
+
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (g Gateway4To6) WorkloadID() int64 {
--- models/generated/workloads/volume.go
+++ models/generated/workloads/volume.go
@@ -1,5 +1,7 @@
 package workloads
 
+import schema "github.com/threefoldtech/tfexplorer/schema"
+
 type Volume struct {
 	WorkloadId      int64             `bson:"workload_id" json:"workload_id"`
 	NodeId          string            `bson:"node_id" json:"node_id"`
@@ -8,6 +10,18 @@ type Volume struct {
 	StatsAggregator []StatsAggregator `bson:"stats_aggregator" json:"stats_aggregator"`
 	FarmerTid       int64             `bson:"farmer_tid" json:"farmer_tid"`
 	PoolId          int64             `bson:"pool_id" json:"pool_id"`
+
+	ID                  schema.ID          `bson:"_id" json:"id"`
+	Json                string             `bson:"json" json:"json"`
+	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
+	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
+	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
+	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
+	SignaturesFarmer    []SigningSignature `bson:"signatures_farmer" json:"signatures_farmer"`
+	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
+	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
+	Metadata            string             `bson:"metadata" json:"metadata"`
+	Results             []Result           `bson:"results" json:"results"`
 }
 
 func (v Volume) WorkloadID() int64 {
