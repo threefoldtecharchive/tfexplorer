48577ca41c2ba1d4fd5b25f1e9fa8dade9c09356
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-06-29 11:32:19 +0200

    add unmarshal bson for workload types

--- models/generated/workloads/workload.go
+++ models/generated/workloads/workload.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pkg/errors"
 	schema "github.com/threefoldtech/tfexplorer/schema"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type (
@@ -99,6 +100,67 @@ func UnmarshalJSON(buffer []byte) (Workloader, error) {
 	case WorkloadTypeZDB:
 		var z ZDB
 		err = json.Unmarshal(buffer, &z)
+		workload = &z
+	default:
+		return nil, errors.New("unrecognized workload type")
+	}
+
+	return workload, err
+}
+
+// UnmarshalBSON decodes a workload from BSON format
+func UnmarshalBSON(buffer []byte) (Workloader, error) {
+	typeField := struct {
+		WorkloadType WorkloadTypeEnum `bson:"workload_type"`
+	}{}
+
+	if err := bson.Unmarshal(buffer, &typeField); err != nil {
+		return nil, errors.Wrap(err, "could not decode workload type")
+	}
+
+	var err error
+	var workload Workloader
+
+	switch typeField.WorkloadType {
+	case WorkloadTypeContainer:
+		var c Container
+		err = bson.Unmarshal(buffer, &c)
+		workload = &c
+	case WorkloadTypeDomainDelegate:
+		var g GatewayDelegate
+		err = bson.Unmarshal(buffer, &g)
+		workload = &g
+	case WorkloadTypeGateway4To6:
+		var g Gateway4To6
+		err = bson.Unmarshal(buffer, &g)
+		workload = &g
+	case WorkloadTypeKubernetes:
+		var k K8S
+		err = bson.Unmarshal(buffer, &k)
+		workload = &k
+	case WorkloadTypeNetworkResource:
+		var n NetworkResource
+		err = bson.Unmarshal(buffer, &n)
+		workload = &n
+	case WorkloadTypeProxy:
+		var g GatewayProxy
+		err = bson.Unmarshal(buffer, &g)
+		workload = &g
+	case WorkloadTypeReverseProxy:
+		var g GatewayReverseProxy
+		err = bson.Unmarshal(buffer, &g)
+		workload = &g
+	case WorkloadTypeSubDomain:
+		var g GatewaySubdomain
+		err = bson.Unmarshal(buffer, &g)
+		workload = &g
+	case WorkloadTypeVolume:
+		var v Volume
+		err = bson.Unmarshal(buffer, &v)
+		workload = &v
+	case WorkloadTypeZDB:
+		var z ZDB
+		err = bson.Unmarshal(buffer, &z)
 		workload = &z
 	default:
 		return nil, errors.New("unrecognized workload type")
