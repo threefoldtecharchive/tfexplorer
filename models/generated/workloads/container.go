631e4243f5fe3c16367e23d5feff0bc4f8a36a8c
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-06-30 11:02:50 +0200

    extract common workload field into it's own struct and embed in actual workloads types

--- models/generated/workloads/container.go
+++ models/generated/workloads/container.go
@@ -1,18 +1,16 @@
 package workloads
 
 import (
-	"encoding/json"
 	"math"
 	"net"
-	"reflect"
-
-	"github.com/pkg/errors"
-	schema "github.com/threefoldtech/tfexplorer/schema"
 )
 
+var _ Workloader = (*Container)(nil)
+var _ Capaciter = (*Container)(nil)
+
 type Container struct {
-	WorkloadId        int64               `bson:"workload_id" json:"workload_id"`
-	NodeId            string              `bson:"node_id" json:"node_id"`
+	ReservationInfo
+
 	Flist             string              `bson:"flist" json:"flist"`
 	HubUrl            string              `bson:"hub_url" json:"hub_url"`
 	Environment       map[string]string   `bson:"environment" json:"environment"`
@@ -23,199 +21,7 @@ type Container struct {
 	NetworkConnection []NetworkConnection `bson:"network_connection" json:"network_connection"`
 	StatsAggregator   []StatsAggregator   `bson:"stats_aggregator" json:"stats_aggregator"`
 	Logs              []Logs              `bson:"logs" json:"logs"`
-	FarmerTid         int64               `bson:"farmer_tid" json:"farmer_tid"`
 	Capacity          ContainerCapacity   `bson:"capcity" json:"capacity"`
-	PoolId            int64               `bson:"pool_id" json:"pool_id"`
-
-	Description             string         `bson:"description" json:"description"`
-	Currencies              []string       `bson:"currencies" json:"currencies"`
-	SigningRequestProvision SigningRequest `bson:"signing_request_provision" json:"signing_request_provision"`
-	SigningRequestDelete    SigningRequest `bson:"signing_request_delete" json:"signing_request_delete"`
-	ExpirationProvisioning  schema.Date    `bson:"expiration_provisioning" json:"expiration_provisioning"`
-
-	ID                  schema.ID          `bson:"_id" json:"id"`
-	Json                string             `bson:"json" json:"json"`
-	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
-	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
-	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
-	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
-	SignatureFarmer     SigningSignature   `bson:"signature_farmer" json:"signature_farmer"`
-	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
-	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
-	Metadata            string             `bson:"metadata" json:"metadata"`
-	Result              Result             `bson:"result" json:"result"`
-	WorkloadType        WorkloadTypeEnum   `bson:"workload_type" json:"workload_type"`
-}
-
-func (c *Container) WorkloadID() int64 {
-	return c.WorkloadId
-}
-
-func (c *Container) GetWorkloadType() WorkloadTypeEnum {
-	return c.WorkloadType
-}
-
-func (c *Container) GetID() schema.ID {
-	return c.ID
-}
-
-func (c *Container) SetID(id schema.ID) {
-	c.ID = id
-}
-
-func (c *Container) GetJson() string {
-	return c.Json
-}
-
-func (c *Container) GetCustomerTid() int64 {
-	return c.CustomerTid
-}
-
-func (c *Container) GetCustomerSignature() string {
-	return c.CustomerSignature
-}
-
-func (c *Container) GetNextAction() NextActionEnum {
-	return c.NextAction
-}
-
-func (c *Container) SetNextAction(next NextActionEnum) {
-	c.NextAction = next
-}
-
-func (c *Container) GetSignaturesProvision() []SigningSignature {
-	return c.SignaturesProvision
-}
-
-func (c *Container) PushSignatureProvision(signature SigningSignature) {
-	c.SignaturesProvision = append(c.SignaturesProvision, signature)
-}
-
-func (c *Container) GetSignatureFarmer() SigningSignature {
-	return c.SignatureFarmer
-}
-
-func (c *Container) SetSignatureFarmer(signature SigningSignature) {
-	c.SignatureFarmer = signature
-}
-
-func (c *Container) GetSignaturesDelete() []SigningSignature {
-	return c.SignaturesDelete
-}
-
-func (c *Container) PushSignatureDelete(signature SigningSignature) {
-	c.SignaturesDelete = append(c.SignaturesDelete, signature)
-}
-
-func (c *Container) GetEpoch() schema.Date {
-	return c.Epoch
-}
-
-func (c *Container) GetMetadata() string {
-	return c.Metadata
-}
-
-func (c *Container) GetResult() Result {
-	return c.Result
-}
-
-func (c *Container) SetResult(result Result) {
-	c.Result = result
-}
-
-func (c *Container) GetDescription() string {
-	return c.Description
-}
-
-func (c *Container) GetCurrencies() []string {
-	return c.Currencies
-}
-
-func (c *Container) GetSigningRequestProvision() SigningRequest {
-	return c.SigningRequestProvision
-}
-
-func (c *Container) GetSigningRequestDelete() SigningRequest {
-	return c.SigningRequestDelete
-}
-
-func (c *Container) GetExpirationProvisioning() schema.Date {
-	return c.ExpirationProvisioning
-}
-
-func (c *Container) SetJson(json string) {
-	c.Json = json
-}
-
-func (c *Container) SetCustomerTid(tid int64) {
-	c.CustomerTid = tid
-}
-
-func (c *Container) SetCustomerSignature(signature string) {
-	c.CustomerSignature = signature
-}
-
-func (c *Container) SetEpoch(date schema.Date) {
-	c.Epoch = date
-}
-
-func (c *Container) SetMetadata(metadata string) {
-	c.Metadata = metadata
-}
-
-func (c *Container) SetDescription(description string) {
-	c.Description = description
-}
-
-func (c *Container) SetCurrencies(currencies []string) {
-	c.Currencies = currencies
-}
-
-func (c *Container) SetSigningRequestProvision(request SigningRequest) {
-	c.SigningRequestProvision = request
-}
-
-func (c *Container) SetSigningRequestDelete(request SigningRequest) {
-	c.SigningRequestDelete = request
-}
-
-func (c *Container) SetExpirationProvisioning(date schema.Date) {
-	c.ExpirationProvisioning = date
-}
-
-func (c *Container) SetSignaturesProvision(signatures []SigningSignature) {
-	c.SignaturesProvision = signatures
-}
-
-func (c *Container) SetSignaturesDelete(signatures []SigningSignature) {
-	c.SignaturesDelete = signatures
-}
-
-func (c *Container) VerifyJSON() error {
-	dup := Container{}
-
-	if err := json.Unmarshal([]byte(c.Json), &dup); err != nil {
-		return errors.Wrap(err, "invalid json data")
-	}
-
-	// override the fields which are not part of the signature
-	dup.ID = c.ID
-	dup.Json = c.Json
-	dup.CustomerTid = c.CustomerTid
-	dup.NextAction = c.NextAction
-	dup.SignaturesProvision = c.SignaturesProvision
-	dup.SignatureFarmer = c.SignatureFarmer
-	dup.SignaturesDelete = c.SignaturesDelete
-	dup.Epoch = c.Epoch
-	dup.Metadata = c.Metadata
-	dup.Result = c.Result
-	dup.WorkloadType = c.WorkloadType
-
-	if match := reflect.DeepEqual(c, dup); !match {
-		return errors.New("json data does not match actual data")
-	}
-
-	return nil
 }
 
 func (c *Container) GetRSU() RSU {
@@ -234,14 +40,6 @@ func (c *Container) GetRSU() RSU {
 	}
 
 	return rsu
-}
-
-func (c *Container) GetPoolID() int64 {
-	return c.PoolId
-}
-
-func (c *Container) GetNodeID() string {
-	return c.NodeId
 }
 
 type ContainerCapacity struct {
--- models/generated/workloads/network_resource.go
+++ models/generated/workloads/network_resource.go
@@ -1,224 +1,24 @@
 package workloads
 
 import (
-	"encoding/json"
-	"reflect"
-
-	"github.com/pkg/errors"
 	schema "github.com/threefoldtech/tfexplorer/schema"
 )
 
+var _ Workloader = (*K8S)(nil)
+var _ Capaciter = (*K8S)(nil)
+
 type NetworkResource struct {
+	ReservationInfo
+
 	Name                         string            `bson:"name" json:"name"`
-	WorkloadId                   int64             `bson:"workload_id" json:"workload_id"`
-	NodeId                       string            `bson:"node_id" json:"node_id"`
 	StatsAggregator              []StatsAggregator `bson:"stats_aggregator" json:"stats_aggregator"`
 	WireguardPrivateKeyEncrypted string            `bson:"wireguard_private_key_encrypted" json:"wireguard_private_key_encrypted"`
 	WireguardPublicKey           string            `bson:"wireguard_public_key" json:"wireguard_public_key"`
 	WireguardListenPort          int64             `bson:"wireguard_listen_port" json:"wireguard_listen_port"`
 	Iprange                      schema.IPRange    `bson:"iprange" json:"iprange"`
 	Peers                        []WireguardPeer   `bson:"peers" json:"peers"`
-	PoolId                       int64             `bson:"pool_id" json:"pool_id"`
-
-	Description             string         `bson:"description" json:"description"`
-	Currencies              []string       `bson:"currencies" json:"currencies"`
-	SigningRequestProvision SigningRequest `bson:"signing_request_provision" json:"signing_request_provision"`
-	SigningRequestDelete    SigningRequest `bson:"signing_request_delete" json:"signing_request_delete"`
-	ExpirationProvisioning  schema.Date    `bson:"expiration_provisioning" json:"expiration_provisioning"`
-	ID                      schema.ID      `bson:"_id" json:"id"`
-
-	Json                string             `bson:"json" json:"json"`
-	CustomerTid         int64              `bson:"customer_tid" json:"customer_tid"`
-	CustomerSignature   string             `bson:"customer_signature" json:"customer_signature"`
-	NextAction          NextActionEnum     `bson:"next_action" json:"next_action"`
-	SignaturesProvision []SigningSignature `bson:"signatures_provision" json:"signatures_provision"`
-	SignatureFarmer     SigningSignature   `bson:"signature_farmer" json:"signature_farmer"`
-	SignaturesDelete    []SigningSignature `bson:"signatures_delete" json:"signatures_delete"`
-	Epoch               schema.Date        `bson:"epoch" json:"epoch"`
-	Metadata            string             `bson:"metadata" json:"metadata"`
-	Result              Result             `bson:"result" json:"result"`
-	WorkloadType        WorkloadTypeEnum   `bson:"workload_type" json:"workload_type"`
-}
-
-func (n *NetworkResource) WorkloadID() int64 {
-	return n.WorkloadId
-}
-
-func (n *NetworkResource) GetWorkloadType() WorkloadTypeEnum {
-	return n.WorkloadType
-}
-
-func (n *NetworkResource) GetID() schema.ID {
-	return n.ID
-}
-
-func (n *NetworkResource) SetID(id schema.ID) {
-	n.ID = id
-}
-
-func (n *NetworkResource) GetJson() string {
-	return n.Json
-}
-
-func (n *NetworkResource) GetCustomerTid() int64 {
-	return n.CustomerTid
-}
-
-func (n *NetworkResource) GetCustomerSignature() string {
-	return n.CustomerSignature
-}
-
-func (n *NetworkResource) GetNextAction() NextActionEnum {
-	return n.NextAction
-}
-
-func (n *NetworkResource) SetNextAction(next NextActionEnum) {
-	n.NextAction = next
-}
-
-func (n *NetworkResource) GetSignaturesProvision() []SigningSignature {
-	return n.SignaturesProvision
-}
-
-func (n *NetworkResource) PushSignatureProvision(signature SigningSignature) {
-	n.SignaturesProvision = append(n.SignaturesProvision, signature)
-}
-
-func (n *NetworkResource) GetSignatureFarmer() SigningSignature {
-	return n.SignatureFarmer
-}
-
-func (n *NetworkResource) SetSignatureFarmer(signature SigningSignature) {
-	n.SignatureFarmer = signature
-}
-
-func (n *NetworkResource) GetSignaturesDelete() []SigningSignature {
-	return n.SignaturesDelete
-}
-
-func (n *NetworkResource) PushSignatureDelete(signature SigningSignature) {
-	n.SignaturesDelete = append(n.SignaturesDelete, signature)
-}
-
-func (n *NetworkResource) GetEpoch() schema.Date {
-	return n.Epoch
-}
-
-func (n *NetworkResource) GetMetadata() string {
-	return n.Metadata
-}
-
-func (n *NetworkResource) GetResult() Result {
-	return n.Result
-}
-
-func (n *NetworkResource) SetResult(result Result) {
-	n.Result = result
-}
-
-func (n *NetworkResource) GetDescription() string {
-	return n.Description
-}
-
-func (n *NetworkResource) GetCurrencies() []string {
-	return n.Currencies
-}
-
-func (n *NetworkResource) GetSigningRequestProvision() SigningRequest {
-	return n.SigningRequestProvision
-}
-
-func (n *NetworkResource) GetSigningRequestDelete() SigningRequest {
-	return n.SigningRequestDelete
-}
-
-func (n *NetworkResource) GetExpirationProvisioning() schema.Date {
-	return n.ExpirationProvisioning
-}
-
-func (n *NetworkResource) SetJson(json string) {
-	n.Json = json
-}
-
-func (n *NetworkResource) SetCustomerTid(tid int64) {
-	n.CustomerTid = tid
-}
-
-func (n *NetworkResource) SetCustomerSignature(signature string) {
-	n.CustomerSignature = signature
-}
-
-func (n *NetworkResource) SetEpoch(date schema.Date) {
-	n.Epoch = date
-}
-
-func (n *NetworkResource) SetMetadata(metadata string) {
-	n.Metadata = metadata
-}
-
-func (n *NetworkResource) SetDescription(description string) {
-	n.Description = description
-}
-
-func (n *NetworkResource) SetCurrencies(currencies []string) {
-	n.Currencies = currencies
-}
-
-func (n *NetworkResource) SetSigningRequestProvision(request SigningRequest) {
-	n.SigningRequestProvision = request
-}
-
-func (n *NetworkResource) SetSigningRequestDelete(request SigningRequest) {
-	n.SigningRequestDelete = request
-}
-
-func (n *NetworkResource) SetExpirationProvisioning(date schema.Date) {
-	n.ExpirationProvisioning = date
-}
-
-func (n *NetworkResource) SetSignaturesProvision(signatures []SigningSignature) {
-	n.SignaturesProvision = signatures
-}
-
-func (n *NetworkResource) SetSignaturesDelete(signatures []SigningSignature) {
-	n.SignaturesDelete = signatures
-}
-
-func (n *NetworkResource) VerifyJSON() error {
-	dup := NetworkResource{}
-
-	if err := json.Unmarshal([]byte(n.Json), &dup); err != nil {
-		return errors.Wrap(err, "invalid json data")
-	}
-
-	// override the fields which are not part of the signature
-	dup.ID = n.ID
-	dup.Json = n.Json
-	dup.CustomerTid = n.CustomerTid
-	dup.NextAction = n.NextAction
-	dup.SignaturesProvision = n.SignaturesProvision
-	dup.SignatureFarmer = n.SignatureFarmer
-	dup.SignaturesDelete = n.SignaturesDelete
-	dup.Epoch = n.Epoch
-	dup.Metadata = n.Metadata
-	dup.Result = n.Result
-	dup.WorkloadType = n.WorkloadType
-
-	if match := reflect.DeepEqual(n, dup); !match {
-		return errors.New("json data does not match actual data")
-	}
-
-	return nil
 }
 
 func (n *NetworkResource) GetRSU() RSU {
 	return RSU{}
-}
-
-func (n *NetworkResource) GetPoolID() int64 {
-	return n.PoolId
-}
-
-func (n *NetworkResource) GetNodeID() string {
-	return n.NodeId
 }
