d27213dd4f533df368e02aac1d70bedb10407190
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-11-04 14:13:28 +0100

    update

--- pkg/stellar/stellar.go
+++ pkg/stellar/stellar.go
@@ -202,7 +202,7 @@ func (w *Wallet) activateEscrowAccount(newKp *keypair.Full, sourceAccount hProto
 	}
 
 	bo := backoff.NewExponentialBackOff()
-	bo.MaxElapsedTime = time.Second * 10 // retry for 5 minutes
+	bo.MaxElapsedTime = time.Second * 20 // retry for 20 seconds maximum
 	bo.MaxInterval = time.Second * 1
 
 	err = backoff.RetryNotify(submitTransactionBackof, bo, func(err error, d time.Duration) {
