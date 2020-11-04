5e9a42c75f3fbce4752292126f5867367ae1b34d
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-11-04 13:39:06 +0100

    lower timeout, add go mod

--- pkg/stellar/stellar.go
+++ pkg/stellar/stellar.go
@@ -202,8 +202,8 @@ func (w *Wallet) activateEscrowAccount(newKp *keypair.Full, sourceAccount hProto
 	}
 
 	bo := backoff.NewExponentialBackOff()
-	bo.MaxElapsedTime = time.Second * 20 // retry for 5 minutes
-	bo.MaxInterval = time.Second * 2
+	bo.MaxElapsedTime = time.Second * 10 // retry for 5 minutes
+	bo.MaxInterval = time.Second * 1
 
 	err = backoff.RetryNotify(submitTransactionBackof, bo, func(err error, d time.Duration) {
 		log.Error().
