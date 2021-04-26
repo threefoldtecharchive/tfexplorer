25b7fc238538b9b2556d40a452a06ec8410b1b0a
Omar Elawady <omarelawady1998@gmail.com>
2021-04-26 11:06:52 +0200

    use size and name instead of capacity and flist

--- models/generated/workloads/vm.go
+++ models/generated/workloads/vm.go
@@ -3,8 +3,6 @@ package workloads
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"math"
 	"net"
 
 	"github.com/rs/zerolog/log"
@@ -17,17 +15,21 @@ var _ Capaciter = (*VirtualMachine)(nil)
 type VirtualMachine struct {
 	ReservationInfo `bson:",inline"`
 
-	Flist     string   `bson:"flist" json:"flist"`
+	Name      string   `bson:"name" json:"name"`
 	NetworkId string   `bson:"network_id" json:"network_id"`
 	Ipaddress net.IP   `bson:"ipaddress" json:"ipaddress"`
 	SshKeys   []string `bson:"ssh_keys" json:"ssh_keys"`
 	// why isn't this a net.IP? because it's a wid
-	PublicIP schema.ID  `bson:"public_ip" json:"public_ip"`
-	Capacity VMCapacity `bson:"capcity" json:"capacity"`
+	PublicIP schema.ID `bson:"public_ip" json:"public_ip"`
+	Size     int64     `bson:"size" json:"size"`
 }
 
-func (vm *VirtualMachine) GetRSU() (RSU, error) {
-	return vm.Capacity.GetRSU()
+func (k *VirtualMachine) GetRSU() (RSU, error) {
+	rsu, ok := k8sSize[k.Size]
+	if !ok {
+		return RSU{}, fmt.Errorf("VM size %d is not supported", k.Size)
+	}
+	return rsu, nil
 }
 
 func (vm *VirtualMachine) SignatureChallenge() ([]byte, error) {
@@ -36,10 +38,10 @@ func (vm *VirtualMachine) SignatureChallenge() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Info().Msg(vm.Flist)
+	log.Info().Msg(vm.Name)
 
 	b := bytes.NewBuffer(ric)
-	if _, err := fmt.Fprintf(b, "%s", vm.Flist); err != nil {
+	if _, err := fmt.Fprintf(b, "%s", vm.Name); err != nil {
 		return nil, err
 	}
 	log.Info().Msg(vm.NetworkId)
@@ -63,41 +65,11 @@ func (vm *VirtualMachine) SignatureChallenge() ([]byte, error) {
 		}
 	}
 
-	if err := vm.Capacity.SigningEncode(b); err != nil {
+	if _, err := fmt.Fprintf(b, "%d", vm.Size); err != nil {
 		return nil, err
 	}
+
 	log.Info().Msg("Entering signature 7")
 	log.Info().Bytes("customer data", b.Bytes()).Msg("Hi")
 	return b.Bytes(), nil
-}
-
-type VMCapacity struct {
-	Cpu      int64  `bson:"cpu" json:"cpu"`
-	Memory   int64  `bson:"memory" json:"memory"`
-	DiskSize uint64 `bson:"disk_size" json:"disk_size"`
-}
-
-func (vm VMCapacity) SigningEncode(w io.Writer) error {
-	if _, err := fmt.Fprintf(w, "%d", vm.Cpu); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprintf(w, "%d", vm.Memory); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprintf(w, "%d", vm.DiskSize); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (vm VMCapacity) GetRSU() (RSU, error) {
-	rsu := RSU{
-		CRU: vm.Cpu,
-		// round mru to 4 digits precision
-		MRU: math.Round(float64(vm.Memory)/1024*10000) / 10000,
-	}
-	storageSize := math.Round(float64(vm.DiskSize)/1024*10000) / 10000
-	storageSize = math.Max(0, storageSize-50) // we offer the 50 first GB of storage for container root
-	rsu.SRU = storageSize
-	return rsu, nil
 }
